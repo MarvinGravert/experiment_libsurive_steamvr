42b8c2a13af4d092db0cd8d6e2aaf56cca0f2693
Marvin Gravert <marvin.gravert@gmail.com>
2021-10-08 22:40:58 +0200

    refactors to general drifting

--- drift_test_libsurvive.py
+++ drift_test_libsurvive.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 from pathlib import Path
@@ -5,12 +6,50 @@ from enum import Enum
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pysurvive
+
+if os.name == 'nt':
+    import openvr
+    import utils.triad_openvr as triad_openvr
+else:
+    import pysurvive
 
 
 class Framework(Enum):
     steamvr = "steamvr"
     libsurvive = "libsurvive"
+
+
+def run_drift_steamvr(frequency: int = 10, duration: float = 10*60) -> np.ndarray:
+    """
+
+    Args:
+        frequency (int, optional): [description]. Defaults to 10.
+        duration (float, optional): [description]. Defaults to 10*60.
+
+    Returns:
+        np.ndarray: duration*frequencyx7 pose_matrix
+    """
+    v = triad_openvr.triad_openvr()
+    v.print_discovered_objects()
+    counter = 0
+    max_counter = duration*frequency
+    pose_matrix = np.zeros((int(max_counter), 7))
+    print("START Measuring")
+    time.sleep(1)
+    pose_list = list()
+    while counter < max_counter:
+        current_time = time.perf_counter()
+        pose = v.devices["tracker_1"].get_pose_quaternion()
+        pose_list.append(pose)
+        try:
+            time_2_sleep = 1/frequency-(time.perf_counter()-current_time)
+            time.sleep(time_2_sleep)
+        except ValueError:  # happends if negative sleep duration (loop took too long)
+            pass
+        counter += 1
+    for j, pose in enumerate(pose_list):
+        pose_matrix[j, :] = np.array(pose)
+    return pose_matrix
 
 
 def run_drift_libsurvive(frequency=10, duration=10*60) -> np.ndarray:
@@ -61,7 +100,6 @@ def run_drift_libsurvive(frequency=10, duration=10*60) -> np.ndarray:
 
 
 def get_file_location(exp_type, exp_num, framework: Framework) -> Path:
-    exp_num = 1
     # file naming stuff
     DATA_PATH = Path("./data")
     FOLDER_PATH = Path(f"{exp_type}/{framework.value}")
@@ -76,30 +114,33 @@ def save_data(file_location: Path, pose_matrix: np.ndarray, settings=dict()):
     current_date_time = time.strftime("%Y%m%d-%H%M%S")
     settings_header = ""
     for key, val in settings.items():
-        settings_header += f"{key} = {val}"
-    header = f"Drift {framework.value}; {current_date_time}; x y z w i j k; {settings_header}\n"
+        settings_header += f" {key} = {val}"
+    header = f"Drift {framework.value}; {current_date_time}; x y z w i j k;{settings_header}\n"
     with file_location.open("w") as file:
         file.write(header)
         np.savetxt(file, pose_matrix)
 
 
 if __name__ == "__main__":
-    exp_num = 1
+    exp_num = 3
     exp_type = "drift"
     # settings:
     settings = {
         "frequency": 10,  # Hz
         "duration": 60*10  # seconds
     }
-    framework = Framework("libsurvive")
+    framework = Framework("steamvr")
     # run program
     if framework == Framework.libsurvive:
         pose_matrix = run_drift_libsurvive(
             frequency=settings["frequency"],
-            duratation=settings["duration"]
+            duration=settings["duration"]
         )
     elif framework == Framework.steamvr:
-        pass
+        pose_matrix = run_drift_steamvr(
+            frequency=settings["frequency"],
+            duration=settings["duration"]
+        )
     else:
         print("framework not recognized")
         exit()
@@ -114,12 +155,13 @@ if __name__ == "__main__":
         framework=framework
     )
     if file_location.exists():
-        exit()
-    save_data(
-        file_location=file_location,
-        pose_matrix=pose_matrix,
-        settings=settings
-    )
+        print("file exists")
+    else:
+        save_data(
+            file_location=file_location,
+            pose_matrix=pose_matrix,
+            settings=settings
+        )
     pos_mean = np.mean(pose_matrix, 0)[:3]
     pos = pose_matrix[:, :3]
     error_pos = np.linalg.norm(pos-pos_mean, axis=1)
