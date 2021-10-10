9ca5a6ebe44f81340b5e739399c2881eb3ff511a
Marvin Gravert <marvin.gravert@gmail.com>
2021-10-11 01:45:05 +0200

    laser_trans_for_steamvr done

--- laser_trans.py
+++ laser_trans.py
@@ -12,6 +12,14 @@ def get_laser_data():
     return temp[:, :3]
 
 
+T_lt2_to_tracker = np.array([[-1, 0, 0, 25],
+                             [0, 1, 0, -85],
+                             [0, 0, -1, 0],
+                             [0, 0, 0, 1]])
+T_tracker_to_lt1 = np.array([[1, 0, 0, 25],
+                             [0, 1, 0, 85],
+                             [0, 0, 1, -9],
+                             [0, 0, 0, 1]])
 if __name__ == "__main__":
     laser_data = get_laser_data()
     first_kos = laser_data[8:11, :]
@@ -19,6 +27,14 @@ if __name__ == "__main__":
     first_T = build_coordinate_system_via_3_points(*first_kos)
     sec_T = build_coordinate_system_via_3_points(*second_kos)
     diff_T = np.linalg.inv(sec_T)@first_T
-    print(diff_T[:3, 3])
-    print(np.linalg.norm(diff_T[:3, 3]))
-    print(distance_between_hom_matrices(first_T, sec_T))
+    # print(diff_T[:3, 3])
+    # print(np.linalg.norm(diff_T[:3, 3]))
+    # print(distance_between_hom_matrices(first_T, sec_T))
+    print(diff_T)
+    """ 
+    1. get the KOS from the laser points
+    2. Calculate the transformation matrix between the KOS
+    3. Calculate the distance from
+    """
+    test = T_lt2_to_tracker@diff_T@T_tracker_to_lt1
+    print(test)
