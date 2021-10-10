aedc41f9589d6d6f52d2efd7732b668801992f98
Marvin Gravert <marvin.gravert@gmail.com>
2021-10-10 14:40:51 +0200

    implements static data

--- static_accuracy.py
+++ static_accuracy.py
@@ -24,11 +24,11 @@ def run_static_accuracy_steamvr(
     x y z w i j k x y z w i j k
 
     Args:
-        frequency (int, optional): [description]. Defaults to 50.
-        duration (float, optional): [description]. Defaults to 10.
+        frequency (int, optional): (max) measuring frequency 
+        duration (float, optional): time during which the tracker are polled
 
     Returns:
-        np.ndarray: [description]
+        np.ndarray: nx14 matrix containing poses of both trackers
     """
     v = triad_openvr.triad_openvr()
     v.print_discovered_objects()
@@ -43,14 +43,72 @@ def run_static_accuracy_steamvr(
         pose_tracker_1 = v.devices["tracker_1"].get_pose_quaternion()
         pose_tracker_2 = v.devices["tracker_2"].get_pose_quaternion()
         pose_list.append([pose_tracker_1, pose_tracker_2])
+        counter += 1
         try:
             time_2_sleep = 1/frequency-(time.perf_counter()-current_time)
             time.sleep(time_2_sleep)
         except ValueError:  # happends if negative sleep duration (loop took too long)
             pass
-        counter += 1
     for j, pose in enumerate(pose_list):
         pose_matrix[j, :] = np.array(pose[0]+pose[1])
+    return pose_matrix
+
+
+def get_pose_libsurvive_obj(pose_obj) -> np.ndarray:
+    pos = np.array([i for i in pose_obj.Pos])  # try np.array(x,dtype=float)
+    rot = np.array([i for i in pose_obj.Rot])
+    return np.hstack((pos, rot))
+
+
+def run_static_accuracy_libsurvive(
+    frequency: int,
+    duration: float,
+) -> np.ndarray:
+    """runs the static accuracy for libsurvive. Collects data from both tracker
+    and writes them into a matrix
+    x y z w i j k x y z w i j k
+
+    Args:
+        frequency (int, optional): (max) measuring frequency 
+        duration (float, optional): time during which the tracker are polled
+    Returns:
+        np.ndarray: nx14 matrix containing poses of both trackers
+    """
+    actx = pysurvive.SimpleContext(sys.argv)
+    time.sleep(5)
+    # collect all objects
+    obj_dict = dict()
+    while actx.Running():
+        updated = actx.NextUpdated()
+        if updated is not None:
+            if updated.Name() not in obj_dict:
+                obj_dict[updated.Name()] = updated
+                print(f"objects: ", updated.Name())
+        if len(obj_dict.keys()) == 4:
+            break
+    counter = 0
+    max_counter = duration*frequency
+    print("START Measuring")
+    time.sleep(1)
+    tracker_obj_1 = obj_dict[b'T20']
+    tracker_obj_2 = obj_dict[b'T21']
+    pose_list = list()
+    while actx.Running() and counter < max_counter:
+        current_time = time.perf_counter()
+        pose_1, _ = tracker_obj_1.Pose()
+        pose_2, _ = tracker_obj_2.Pose()
+        pose_list.append([pose_1, pose_2])
+        counter += 1
+        try:
+            time_2_sleep = 1/frequency-(time.perf_counter()-current_time)
+            time.sleep(time_2_sleep)
+        except ValueError:  # happends if negative sleep duration (loop took too long)
+            pass
+    pose_matrix = np.zeros((int(max_counter), 7))
+    for j, (pose_1, pose_2) in enumerate(pose_list):
+        pose_1 = get_pose_libsurvive_obj(pose_1)
+        pose_2 = get_pose_libsurvive_obj(pose_2)
+        pose_matrix[j, :] = np.hstack((pose_1, pose_2))
     return pose_matrix
 
 
