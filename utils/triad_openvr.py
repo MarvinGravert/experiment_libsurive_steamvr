07bec357870b08a33a00b3f2aed8702bf30e1ced
Marvin Gravert <marvin.gravert@gmail.com>
2021-10-23 02:33:22 +0200

    adds comment to triad openvr

--- utils/triad_openvr.py
+++ utils/triad_openvr.py
@@ -1,3 +1,6 @@
+"""TRIAD_OPENVR: human readable python api for steamvr
+FIND HERE: https://github.com/TriadSemi/triad_openvr
+"""
 import time
 import sys
 import openvr
@@ -7,11 +10,15 @@ import json
 from functools import lru_cache
 
 # Function to print out text but instead of starting a new line it will overwrite the existing line
+
+
 def update_text(txt):
     sys.stdout.write('\r'+txt)
     sys.stdout.flush()
 
-#Convert the standard 3x4 position/rotation matrix to a x,y,z location and the appropriate Euler angles (in degrees)
+# Convert the standard 3x4 position/rotation matrix to a x,y,z location and the appropriate Euler angles (in degrees)
+
+
 def convert_to_euler(pose_mat):
     yaw = 180 / math.pi * math.atan2(pose_mat[1][0], pose_mat[0][0])
     pitch = 180 / math.pi * math.atan2(pose_mat[2][0], pose_mat[0][0])
@@ -19,9 +26,11 @@ def convert_to_euler(pose_mat):
     x = pose_mat[0][3]
     y = pose_mat[1][3]
     z = pose_mat[2][3]
-    return [x,y,z,yaw,pitch,roll]
+    return [x, y, z, yaw, pitch, roll]
 
-#Convert the standard 3x4 position/rotation matrix to a x,y,z location and the appropriate Quaternion
+# Convert the standard 3x4 position/rotation matrix to a x,y,z location and the appropriate Quaternion
+
+
 def convert_to_quaternion(pose_mat):
     # Per issue #2, adding a abs() so that sqrt only results in real numbers
     r_w = math.sqrt(abs(1+pose_mat[0][0]+pose_mat[1][1]+pose_mat[2][2]))/2
@@ -32,9 +41,11 @@ def convert_to_quaternion(pose_mat):
     x = pose_mat[0][3]
     y = pose_mat[1][3]
     z = pose_mat[2][3]
-    return [x,y,z,r_w,r_x,r_y,r_z]
+    return [x, y, z, r_w, r_x, r_y, r_z]
 
-#Define a class to make it easy to append pose matricies and convert to both Euler and Quaternion for plotting
+# Define a class to make it easy to append pose matricies and convert to both Euler and Quaternion for plotting
+
+
 class pose_sample_buffer():
     def __init__(self):
         self.i = 0
@@ -51,26 +62,29 @@ class pose_sample_buffer():
         self.r_y = []
         self.r_z = []
 
-    def append(self,pose_mat,t):
+    def append(self, pose_mat, t):
         self.time.append(t)
         self.x.append(pose_mat[0][3])
         self.y.append(pose_mat[1][3])
         self.z.append(pose_mat[2][3])
-        self.yaw.append(180 / math.pi * math.atan(pose_mat[1][0] /pose_mat[0][0]))
-        self.pitch.append(180 / math.pi * math.atan(-1 * pose_mat[2][0] / math.sqrt(pow(pose_mat[2][1], 2) + math.pow(pose_mat[2][2], 2))))
-        self.roll.append(180 / math.pi * math.atan(pose_mat[2][1] /pose_mat[2][2]))
+        self.yaw.append(180 / math.pi * math.atan(pose_mat[1][0] / pose_mat[0][0]))
+        self.pitch.append(
+            180 / math.pi * math.atan(-1 * pose_mat[2][0] / math.sqrt(pow(pose_mat[2][1], 2) + math.pow(pose_mat[2][2], 2))))
+        self.roll.append(180 / math.pi * math.atan(pose_mat[2][1] / pose_mat[2][2]))
         r_w = math.sqrt(abs(1+pose_mat[0][0]+pose_mat[1][1]+pose_mat[2][2]))/2
         self.r_w.append(r_w)
         self.r_x.append((pose_mat[2][1]-pose_mat[1][2])/(4*r_w))
         self.r_y.append((pose_mat[0][2]-pose_mat[2][0])/(4*r_w))
         self.r_z.append((pose_mat[1][0]-pose_mat[0][1])/(4*r_w))
 
+
 def get_pose(vr_obj):
-    return vr_obj.getDeviceToAbsoluteTrackingPose(openvr.TrackingUniverseStanding, 0, openvr.k_unMaxTrackedDeviceCount)
+    return vr_obj.getDeviceToAbsoluteTrackingPose(
+        openvr.TrackingUniverseStanding, 0, openvr.k_unMaxTrackedDeviceCount)
 
 
 class vr_tracked_device():
-    def __init__(self,vr_obj,index,device_class):
+    def __init__(self, vr_obj, index, device_class):
         self.device_class = device_class
         self.index = index
         self.vr = vr_obj
@@ -83,22 +97,22 @@ class vr_tracked_device():
         return self.vr.getStringTrackedDeviceProperty(self.index, openvr.Prop_ModelNumber_String)
 
     def get_battery_percent(self):
-        return self.vr.getFloatTrackedDeviceProperty(self.index, openvr.Prop_DeviceBatteryPercentage_Float)
+        return self.vr.getFloatTrackedDeviceProperty(
+            self.index, openvr.Prop_DeviceBatteryPercentage_Float)
 
     def is_charging(self):
         return self.vr.getBoolTrackedDeviceProperty(self.index, openvr.Prop_DeviceIsCharging_Bool)
 
-
-    def sample(self,num_samples,sample_rate):
+    def sample(self, num_samples, sample_rate):
         interval = 1/sample_rate
         rtn = pose_sample_buffer()
         sample_start = time.time()
         for i in range(num_samples):
             start = time.time()
             pose = get_pose(self.vr)
-            rtn.append(pose[self.index].mDeviceToAbsoluteTracking,time.time()-sample_start)
-            sleep_time = interval- (time.time()-start)
-            if sleep_time>0:
+            rtn.append(pose[self.index].mDeviceToAbsoluteTracking, time.time()-sample_start)
+            sleep_time = interval - (time.time()-start)
+            if sleep_time > 0:
                 time.sleep(sleep_time)
         return rtn
 
@@ -179,13 +193,17 @@ class vr_tracked_device():
         """
         Causes devices with haptic feedback to vibrate for a short time.
         """
-        self.vr.triggerHapticPulse(self.index ,axis_id, duration_micros)
+        self.vr.triggerHapticPulse(self.index, axis_id, duration_micros)
+
 
 class vr_tracking_reference(vr_tracked_device):
     def get_mode(self):
-        return self.vr.getStringTrackedDeviceProperty(self.index,openvr.Prop_ModeLabel_String).decode('utf-8').upper()
-    def sample(self,num_samples,sample_rate):
+        return self.vr.getStringTrackedDeviceProperty(
+            self.index, openvr.Prop_ModeLabel_String).decode('utf-8').upper()
+
+    def sample(self, num_samples, sample_rate):
         print("Warning: Tracking References do not move, sample isn't much use...")
+
 
 class triad_openvr():
     def __init__(self, configfile_path=None):
@@ -194,30 +212,32 @@ class triad_openvr():
         self.vrsystem = openvr.VRSystem()
 
         # Initializing object to hold indexes for various tracked objects
-        self.object_names = {"Tracking Reference":[],"HMD":[],"Controller":[],"Tracker":[]}
+        self.object_names = {"Tracking Reference": [], "HMD": [], "Controller": [], "Tracker": []}
         self.devices = {}
         self.device_index_map = {}
         poses = self.vr.getDeviceToAbsoluteTrackingPose(openvr.TrackingUniverseStanding, 0,
-                                                               openvr.k_unMaxTrackedDeviceCount)
+                                                        openvr.k_unMaxTrackedDeviceCount)
 
         # Loading config file
         if configfile_path:
             try:
                 with open(configfile_path, 'r') as json_data:
                     config = json.load(json_data)
-            except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+            except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
                 print('config.json not found.')
                 exit(1)
 
             # Iterate through the pose list to find the active devices and determine their type
             for i in range(openvr.k_unMaxTrackedDeviceCount):
                 if poses[i].bDeviceIsConnected:
-                    device_serial = self.vr.getStringTrackedDeviceProperty(i,openvr.Prop_SerialNumber_String).decode('utf-8')
+                    device_serial = self.vr.getStringTrackedDeviceProperty(
+                        i, openvr.Prop_SerialNumber_String).decode('utf-8')
                     for device in config['devices']:
                         if device_serial == device['serial']:
                             device_name = device['name']
                             self.object_names[device['type']].append(device_name)
-                            self.devices[device_name] = vr_tracked_device(self.vr,i,device['type'])
+                            self.devices[device_name] = vr_tracked_device(
+                                self.vr, i, device['type'])
         else:
             # Iterate through the pose list to find the active devices and determine their type
             for i in range(openvr.k_unMaxTrackedDeviceCount):
@@ -239,7 +259,7 @@ class triad_openvr():
             if event.eventType == openvr.VREvent_TrackedDeviceActivated:
                 self.add_tracked_device(event.trackedDeviceIndex)
             elif event.eventType == openvr.VREvent_TrackedDeviceDeactivated:
-                #If we were already tracking this device, quit tracking it.
+                # If we were already tracking this device, quit tracking it.
                 if event.trackedDeviceIndex in self.device_index_map:
                     self.remove_tracked_device(event.trackedDeviceIndex)
 
@@ -249,22 +269,22 @@ class triad_openvr():
         if (device_class == openvr.TrackedDeviceClass_Controller):
             device_name = "controller_"+str(len(self.object_names["Controller"])+1)
             self.object_names["Controller"].append(device_name)
-            self.devices[device_name] = vr_tracked_device(self.vr,i,"Controller")
+            self.devices[device_name] = vr_tracked_device(self.vr, i, "Controller")
             self.device_index_map[i] = device_name
         elif (device_class == openvr.TrackedDeviceClass_HMD):
             device_name = "hmd_"+str(len(self.object_names["HMD"])+1)
             self.object_names["HMD"].append(device_name)
-            self.devices[device_name] = vr_tracked_device(self.vr,i,"HMD")
+            self.devices[device_name] = vr_tracked_device(self.vr, i, "HMD")
             self.device_index_map[i] = device_name
         elif (device_class == openvr.TrackedDeviceClass_GenericTracker):
             device_name = "tracker_"+str(len(self.object_names["Tracker"])+1)
             self.object_names["Tracker"].append(device_name)
-            self.devices[device_name] = vr_tracked_device(self.vr,i,"Tracker")
+            self.devices[device_name] = vr_tracked_device(self.vr, i, "Tracker")
             self.device_index_map[i] = device_name
         elif (device_class == openvr.TrackedDeviceClass_TrackingReference):
             device_name = "tracking_reference_"+str(len(self.object_names["Tracking Reference"])+1)
             self.object_names["Tracking Reference"].append(device_name)
-            self.devices[device_name] = vr_tracking_reference(self.vr,i,"Tracking Reference")
+            self.devices[device_name] = vr_tracking_reference(self.vr, i, "Tracking Reference")
             self.device_index_map[i] = device_name
 
     def remove_tracked_device(self, tracked_device_index):
@@ -274,9 +294,10 @@ class triad_openvr():
             del self.device_index_map[tracked_device_index]
             del self.devices[device_name]
         else:
-            raise Exception("Tracked device index {} not valid. Not removing.".format(tracked_device_index))
+            raise Exception("Tracked device index {} not valid. Not removing.".format(
+                tracked_device_index))
 
-    def rename_device(self,old_device_name,new_device_name):
+    def rename_device(self, old_device_name, new_device_name):
         self.devices[new_device_name] = self.devices.pop(old_device_name)
         for i in range(len(self.object_names[self.devices[new_device_name].device_class])):
             if self.object_names[self.devices[new_device_name].device_class][i] == old_device_name:
@@ -285,15 +306,15 @@ class triad_openvr():
     def print_discovered_objects(self):
         for device_type in self.object_names:
             plural = device_type
-            if len(self.object_names[device_type])!=1:
-                plural+="s"
+            if len(self.object_names[device_type]) != 1:
+                plural += "s"
             print("Found "+str(len(self.object_names[device_type]))+" "+plural)
             for device in self.object_names[device_type]:
                 if device_type == "Tracking Reference":
-                    print("  "+device+" ("+self.devices[device].get_serial()+
-                          ", Mode "+self.devices[device].get_model()+
-                          ", "+self.devices[device].get_model()+
+                    print("  "+device+" ("+self.devices[device].get_serial() +
+                          ", Mode "+self.devices[device].get_model() +
+                          ", "+self.devices[device].get_model() +
                           ")")
                 else:
-                    print("  "+device+" ("+self.devices[device].get_serial()+
+                    print("  "+device+" ("+self.devices[device].get_serial() +
                           ", "+self.devices[device].get_model()+")")
